7b68afffb390ce7f6775692f13e3c45c91250c2d
reece palmer <reecelpalmer@gmail.com>
2023-10-20 10:50:58 +0100

    Progress on 5 tests passing

--- school.py
+++ school.py
@@ -1,15 +1,14 @@
 # pylint:disable=C0111,C0103
 
-def students_from_city(db, city):
+import sqlite3
+conn = sqlite3.connect('data/school.sqlite')
+db = conn.cursor()
+
+
+def students_from_city(db_cursor, city):
     """return a list of students from a specific city"""
-    pass  # YOUR CODE HERE
+    query = "SELECT * FROM students WHERE birth_city = ?"
+    db_cursor.execute(query, (city,))
+    return db_cursor.fetchall()
 
-
-# To test your code, you can **run it** before running `make`
-#   => Uncomment the following lines + run:
-#   $ python school.py
-#
-# import sqlite3
-# conn = sqlite3.connect('data/school.sqlite')
-# db = conn.cursor()
-# print(students_from_city(db, 'Paris'))
+print(students_from_city(db, 'Paris'))
